ca30d6a42fb9c7ec53867cf05795a1db216b75c0
banksjim <jimbankshome@gmail.com>
2024-02-04 13:14:39 -0700

    Basic Python data types

---
+++ Day-2/data_types.py
@@ -0,0 +1,20 @@
+# Data Types
+
+# String
+print("Hello"[4]) # In Python, the first character is always 0 and you can print specific characters from a string by using the []
+print("123"[1])
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+print(123_456_789) # In Python, we can use underscore to make the number more readable
+
+# Float
+
+pi = 3.14159
+print(pi)
+
+# Boolean
+
+is_active = True
+print(is_active)
