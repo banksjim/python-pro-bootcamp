9ebcce336af386083d06351a6d1844a533bc0179
banksjim <jimbankshome@gmail.com>
2024-02-04 13:58:28 -0700

    Python math order of operations

---
+++ Day-2/math_operators.py
@@ -0,0 +1,14 @@
+print(3 + 5)
+print(7 - 4)
+print(3 * 2)
+print(6 / 3) # Division in Python always results in a floating point number
+print(2 ** 3)
+
+# PEMDAS Order of operations:
+# Parentheses ()
+# Exponents **
+# Multiplication * & # Division /
+# Addition + & # Subtraction - 
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * ((3 + 3) / 3) - 3)
