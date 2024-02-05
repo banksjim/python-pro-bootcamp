c8bad09658c9fbb4845400afe9aa9dc786df1076
Jim Banks <jimbankshome@gmail.com>
2024-02-05 16:41:54 -0700

    Using shorthand math operators

--- Day-2/more_number_fun.py
+++ Day-2/more_number_fun.py
@@ -1,3 +1,14 @@
+# Rounding numbers with specified precision
 print(round((8 / 3), 2)) # rounding a result instead of truncating with int
 
+# Floored division operator
 print(8 // 3) # Floor division "//" automatically converts the final value to an int
+
+# Shorthand operators include -= += *= /= etc.
+result = 4 / 2
+result /= 2 # Same as "result = result / 2"
+print(result)
+
+score = 0
+score += 1
+print(score)
