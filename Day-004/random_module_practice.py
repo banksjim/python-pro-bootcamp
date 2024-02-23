01018b2acddca6b3b112aefd59667c0484775fca
Jim Banks <jimbankshome@gmail.com>
2024-02-23 15:34:36 -0700

    Intro to custom modules

--- Day-004/random_module_practice.py
+++ Day-004/random_module_practice.py
@@ -1,5 +1,10 @@
 import random
+import my_module
 
+# Random integer generation practice
 for counter in range(1, 10):
     random_integer = random.randint(1, 10)
     print(random_integer)
+
+# Module practice
+print(my_module.pi)
