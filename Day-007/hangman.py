1b5cfb0bc3ecaf630e702679651e875ea0b17eaa
Jim Banks <jimbankshome@gmail.com>
2024-03-28 17:18:43 -0600

    Call herokuapp API to retrieve a random word

--- Day-007/hangman.py
+++ Day-007/hangman.py
@@ -1,3 +1,5 @@
+import requests
+
 from shared_modules.system_modules import clear_terminal
 
 # main() program logic module
@@ -7,11 +9,12 @@ def main():
     clear_terminal()
 
     # initialize variables
-    end_game: bool = False
-    guess:    str = ""
+    end_game:    bool = False
+    guess:       str = ""
+    random_word: str = ""
 
     # ASCII art variables
-    banner:   str = r'''
+    banner:      str = r'''
     .__                                                 
     |  |__ _____    ____    ____   _____ _____    ____  
     |  |  \\__  \  /    \  / ___\ /     \\__  \  /    \ 
@@ -35,16 +38,25 @@ def main():
         '   O   |',
         '   |   |',
         '  /|   |',
-        '  /|\  |',
+        '  /|\\  |',
         '  /    |',
-        '  / \  |'
+        '  / \\  |'
     ]
 
     # mainline statements
     print(banner)
 
     while not end_game:
-        guess = input("Guess a letter: ")
+        # guess = input("Guess a letter: ")
+
+        # generate a random word via API call
+        # response = requests.get('http://random-word-api.herokuapp.com/word?number=1', timeout=5)
+        # if response.status_code == 200:
+        #     random_word = response.json()[0]
+        # else:
+        #     print(f'Error: {response.status_code}')
+
+        end_game = True
 
     return None
 
