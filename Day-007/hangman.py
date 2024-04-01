1490378051b6ec2bbf0bf917f36753bf5953d6e5
Jim Banks <jimbankshome@gmail.com>
2024-04-01 16:16:18 -0600

    WIP - Pseudo coding out the hangman solution

--- Day-007/hangman.py
+++ Day-007/hangman.py
@@ -11,6 +11,7 @@ def main():
     # initialize variables
     end_game:    bool = False
     guess:       str = ""
+    hanged_man:  bool = False
     random_word: str = ""
 
     # ASCII art variables
@@ -33,7 +34,7 @@ def main():
         '       |',
         '=========='
     ]
-    
+ 
     hanged_man = [
         '   O   |',
         '   |   |',
@@ -55,6 +56,32 @@ def main():
         #     random_word = response.json()[0]
         # else:
         #     print(f'Error: {response.status_code}')
+
+    # count the number of letters in the random word
+
+    # store the letters of the random word in a list of letters
+
+    # store the partially completed word in a list of letters starting with blanks in each character
+
+    # prompt for guess the first letter letter
+
+    # loop user guesses until the random word is guessed or the hanged man flag is true
+
+        # clear the terminal screen
+
+        # show the partially completed world list with each letter separated by a space
+
+        # user guesses a letter
+
+        # check if the guessed letter is in the word
+
+            # yes - fill the letter in all matching locations of the partially completed word list
+
+                # check if any of the remaining characters are blanks
+
+                    # no - the word was guessed correctly and end game = true
+
+            # no - add a level to the hanged man, and increase the hanged level count
 
         end_game = True
 
