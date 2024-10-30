4dd84e53171ee03d676536740494694beb19d8b8
Jim Banks <jimbankshome@gmail.com>
2024-10-29 21:02:52 -0600

    Declaring constants in python

---
+++ Day-012/constants.py
@@ -0,0 +1,11 @@
+# Global constants
+
+PI = 3.1459 # While not required, convention is to use all UPPER CASE
+GOOGLE_URL = 'https://www.google.com'
+
+def myfunc():
+    print(GOOGLE_URL)
+    print(PI)
+    return None
+    
+myfunc()    
