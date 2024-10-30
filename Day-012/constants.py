36be50783b54bb89f6a7b25f99f3e97c34444abe
Jim Banks <jimbankshome@gmail.com>
2024-10-29 21:44:05 -0600

    Make the constants lesson example more readable

--- Day-012/constants.py
+++ Day-012/constants.py
@@ -3,9 +3,9 @@
 PI = 3.1459 # While not required, convention is to use all UPPER CASE
 GOOGLE_URL = 'https://www.google.com'
 
-def myfunc():
+def my_function():
     print(GOOGLE_URL)
     print(PI)
     return None
     
-myfunc()    
+my_function()    
