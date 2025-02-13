3a7fa9582d6f98be8f878ac9ad9c5915f90ba73a
Jim Banks <jimbankshome@gmail.com>
2025-02-13 13:12:01 -0700

    Create and populate the coffee machine config data

---
+++ Day-015/coffee_machine_config_data.py
@@ -0,0 +1,32 @@
+menu = {
+    'espresso': {
+        'ingredients': {
+            'water': 50,
+            'coffee': 18,
+            'milk': 0            
+        },
+        'cost': 1.50
+    },
+    'latte': {
+        'ingredients': {
+            'water': 200,
+            'coffee': 24,
+            'milk': 150            
+        },
+        'cost': 2.50
+    },
+    'cappuccino': {
+        'ingredients': {
+            'water': 250,
+            'coffee': 24,
+            'milk': 100            
+        },
+        'cost': 3.00
+    }
+}
+
+resources = {
+    'water': 300,
+    'coffee': 200,
+    'milk': 100       
+}
