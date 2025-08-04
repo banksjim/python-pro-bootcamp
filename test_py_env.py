47947b44b3c0cb2133a73a229fff6bf3cd5348dd
Jim <jimbankshome@gmail.com>
2025-08-04 11:18:04 -0600

    Small py script to test that VS Code is using the Python virtual environment

---
+++ test_py_env.py
@@ -0,0 +1,5 @@
+# This script should return the directory of your virtual environment
+# if VS Code is using it correctly. Example: "/home/banksjim/.venvs/py3135env/bin/python".
+
+import sys
+print(sys.executable)
